e286f4609f6b0efd824c3d9e9cf8cf2fce8cb51b
vinybag <vinybag@gmail.com>
2025-09-22 09:09:49 -0300

    Adiciona carregamento automático dos assentos no deploy

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
-# Fail on error
+# render-build.sh
+
+# Interrompe em caso de erro
 set -o errexit  
+
+echo "===> Instalando dependências"
+pip install -r requirements.txt
 
 echo "===> Rodando collectstatic"
 python manage.py collectstatic --noinput
 
 echo "===> Rodando migrations"
 python manage.py migrate --noinput
+
+echo "===> Carregando assentos"
+python manage.py loaddata venda_cadeiras/assentos_completos.json || true
