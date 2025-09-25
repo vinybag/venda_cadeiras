ad65d8deb6b0346d85a846d66bec1e89710c4977
vinybag <vinybag@gmail.com>
2025-09-25 18:56:02 -0300

    Remover duplicado do render-build.sh

--- venda_cadeiras/render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Fail on error
-set -o errexit  
-
-echo "===> Rodando collectstatic"
-python manage.py collectstatic --noinput
-
-echo "===> Rodando migrations"
-python manage.py migrate --noinput
